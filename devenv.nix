2a96334218b54343b5acb0fac25dc14ee7fde787
Islam Ahmed <softeng.islam@gmail.com>
2025-08-03 01:05:58 +0300

    updates & Improvements

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # languages.javascript.bun.install.enable = false;
+  languages.typescript.enable = true;
+  languages.javascript.enable = true;
+  languages.javascript.corepack.enable = true;
+  languages.javascript.pnpm.enable = true;
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+}
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vuePugPlugin from "vue-pug-plugin";
+import { VitePluginNode } from "vite-plugin-node";
+
+// https://vite.dev/config/
+export default defineConfig({
+	base: "./",
+	server: {
+		// vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
+		port: 3000,
+	},
+	plugins: [
+		vue({
+			template: {
+				preprocessOptions: {
+					plugins: [vuePugPlugin],
+				},
+			},
+		}),
+	],
+});
